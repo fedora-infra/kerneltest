8ec9a633c86ded197c48d29f753fcb246aa393be
Justin M. Forbes <jforbes@fedoraproject.org>
2024-03-21 19:24:15 -0500

    Update rootest.sh to most recent version being run
    Signed-off-by: Justin M. Forbes <jforbes@fedoraproject.org>

--- guests/rootest.sh
+++ guests/rootest.sh
@@ -1,16 +1,23 @@
-#!/bin/bash
+#!/bin/bash -v
 #
 # Licensed under the terms of the GNU GPL License version 2
 
-FedoraRelease=Fedora20
+FedoraRelease=Fedora40
 currentkernel=kernel-`uname -r`
+kojidir=/home/kerneltest/koji/
+kerneltestdir=/home/kerneltest/kernel-tests/
+
+# Just gives us time to log in and kill it for maintenance
+sleep 90
+
+#Update Guest
+dnf -y update
+
 if [ ! -f /data/latest/$FedoraRelease ]; then
     mount /data
 fi
+
 latestkernel=`cat /data/latest/$FedoraRelease`
-kojidir=/home/kerneltest/koji/
-kerneltestdir=/home/kerneltest/kernel-tests/
-trinitydir=/home/kerneltest/trinity/
 
 #Make sure we are on the latest kernel, install if not
 if [ "$currentkernel" != "$latestkernel.x86_64" ]
@@ -18,18 +25,20 @@ then
     cd $kojidir
     rm *.rpm
     koji download-build --arch=x86_64 $latestkernel
-    rm *debug*.rpm
-    yum -y update *.rpm
+    dnf -y update *.rpm
     reboot
 fi
 
 #We are on the latest kernel, run some tests
 cd $kerneltestdir
-
+git pull
+sleep 120
 #Regression Test as root
 ./runtests.sh
 if [ "$result" != "0" ]
 then
-    echo "Regression Test Suite fail for kernel $currentkernel" >> /data/logs/$FedoraRelease
+    echo "Regression Test Suite fail for kernel $currentkernel"
+    ./runtests.sh
 fi
+
 /usr/sbin/shutdown now -h
